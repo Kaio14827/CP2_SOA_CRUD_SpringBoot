496ab6ac27c09a58653b0e53725cd57a775e6bef
Kaio14827 <163008273+Kaio14827@users.noreply.github.com>
2026-04-24 17:55:02 -0300

    Delete Checkpoint2 directory

--- Checkpoint2/src/main/java/br/com/fiap/checkpoint2/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.checkpoint2.repository;
-
-import br.com.fiap.checkpoint2.model.OrderModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<OrderModel, Long> {
-}
